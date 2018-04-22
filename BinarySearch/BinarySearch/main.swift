c551aadbd0c011f006b8f1391ec6e5fdfe121352
seoju <wngus606@gmail.com>
2018-04-23 01:37:06 +0900

    add message

--- BinarySearch/BinarySearch/main.swift
+++ BinarySearch/BinarySearch/main.swift
@@ -25,3 +25,5 @@ func binarySearch<T: Comparable>(list: [T], item: T) -> Int? {
 let myList = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
 
 print(binarySearch(list: myList, item: 5) ?? "nil")
+
+// O(log n)
